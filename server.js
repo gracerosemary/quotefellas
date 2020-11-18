bc16dc37ee1bb0b65308461d44d5bcfda3770ac2
Klace Koch <koch.klace@gmail.com>
2020-11-17 21:13:37 -0800

    Completed Sunny API call, Simpsons still needs work

--- server.js
+++ server.js
@@ -17,14 +17,48 @@ app.set('view engine', 'ejs');
 
 const client = new pg.Client(process.env.DATABASE_URL);
 
+// Sunny Route
+app.get('/sunny', sunnyQuotes);
 
+function sunnyQuotes (req, res){
+  let API = 'http://sunnyquotes.net/q.php?random';
+
+  superagent.get(API).then( data => {
+    let newSunny = new Sunny(data.body);
+    // res.status(200).send(newSunny);
+    console.log(newSunny);
+  });
+  // .catch(error => console.log(error));
+}
+// Sunny Constructor
+function Sunny(newSunnyQuote){
+  this.quote = newSunnyQuote.sqQuote;
+  this.quoter = newSunnyQuote.sqWho;
+}
+// ----------------------------------------
+// Simpson Route
+app.get('/simpsons', simpsonQuotes);
+function simpsonQuotes (req, res){
+  let API = 'https://thesimpsonsquoteapi.glitch.me/quotes';
+
+  superagent.get(API).then(data => {
+    console.log(data.body);
+    let newSimpson = new Simpson(data.body);
+    console.log(newSimpson);
+  });
+}
+// Simpson Constructor
+function Simpson(newSimpsonQuote){
+  this.quote = newSimpsonQuote.quote;
+  this.quoter= newSimpsonQuote.character;
+  // currently throws UnhandledPromiseRejectionWarning - probably just needs catch block
+}
 
 client.connect().then(()=> {
   app.listen(port, () => {
-    console.log("its alive");
-  })
+    console.log('its alive');
+  });
 })
-.catch (error => {
-  console.log(error);
-});
-
+  .catch (error => {
+    console.log(error);
+  });
