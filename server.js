b2f3ea855501fcd01a63a03b3c9658cbceac9014
scottfalbo <scottfalboart@gmail.com>
2020-11-17 20:03:33 -0800

    office api

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 'use strict';
 
+//dependencies
 const express = require ('express');
 const superagent = require ('superagent');
 const cors = require ('cors');
@@ -7,24 +8,51 @@ const pg = require ('pg');
 const methodOverride = require ('method-override');
 require ('dotenv').config();
 
+//port setup
 const port = process.env.PORT || 3000;
+//turning things on
 const app = express();
 app.use(methodOverride('_method'));
 app.use(express.static('./public'));
 app.use(express.urlencoded({extended:true}));
 app.use(cors());
 app.set('view engine', 'ejs');
-
+//database set up
 const client = new pg.Client(process.env.DATABASE_URL);
 
+app.get('/office', officeAPI);
 
 
-client.connect().then(()=> {
-  app.listen(port, () => {
-    console.log("its alive");
+
+
+
+function officeAPI(req, res){
+  console.log('thats what she said');
+  const URL = `https://officeapi.dev/api/quotes/random`;
+  superagent.get(URL)
+    .then(data => {
+      let quote = new Office((JSON.parse(data.text)).data);
+      console.log(quote);
+      res.status(200).send(quote);
+    })
+    .catch(error => console.log(error));
+}
+function Office(obj){
+  this.quote = obj.content;
+  let name = `${obj.character.firstname} ${obj.character.lastname}`;
+  this.quoter = name;
+}
+
+
+
+
+// turn the server on
+client.connect()
+  .then(()=> {
+    app.listen(port, () => {
+      console.log('its alive');
+    });
   })
-})
-.catch (error => {
-  console.log(error);
-});
-
+  .catch (error => {
+    console.log(error);
+  });
