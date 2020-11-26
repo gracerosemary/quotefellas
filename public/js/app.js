98a7589e876af44247d32dd06249332cf7b3d1a8
scottfalbo <scottfalboart@gmail.com>
2020-11-25 16:16:13 -0800

    fixed submit button bug

--- public/js/app.js
+++ public/js/app.js
@@ -10,30 +10,32 @@ $(() => {
 // submit button.  Handles the front end stuff and then shows button to call /answer route
 $(() => {
   $('#submit-answer').click(() => {
-    let answer = $('input[name=answer]:checked').val();
-    let correct = $('input[name=correct]').val();
-    $('.radios').addClass('click-thru'); // lock answer in by making element click through
-    $('#submit-answer').fadeToggle(1000); // remove submit button
-    $('#next-question').fadeToggle(1000); //show continue button
-    // if the answer is incorrect highlight in red
-    if(answer !== correct){
-      $(`input[name=answer]:checked`).parent().addClass('incorrect-answer');
-      let children = $('input[name=answer]');
-      //highlight the correct answer in green
-      for (let i = 0; i < 4; i++){
-        if (children[i].value === correct){
-          $(`#${children[i].id}`).parent().addClass('correct-answer');
+    if($('input').is(':checked')){ // makes sure an answer is selected
+      let answer = $('input[name=answer]:checked').val();
+      let correct = $('input[name=correct]').val();
+      $('.radios').addClass('click-thru'); // lock answer in by making element click through
+      $('#submit-answer').fadeToggle(1000); // remove submit button
+      $('#next-question').fadeToggle(1000); //show continue button
+      // if the answer is incorrect highlight in red
+      if(answer !== correct){
+        $(`input[name=answer]:checked`).parent().addClass('incorrect-answer');
+        let children = $('input[name=answer]');
+        //highlight the correct answer in green
+        for (let i = 0; i < 4; i++){
+          if (children[i].value === correct){
+            $(`#${children[i].id}`).parent().addClass('correct-answer');
+          }
         }
+        $('#next-question').text('Incorrect >>');
       }
-      $('#next-question').text('Incorrect >>');
-    }
-    else {
-      $(`input[name=answer]:checked`).parent().addClass('correct-answer');
-      // update score board in real time before route call
-      let score = $('#player-score').text();
-      score++;
-      $('#player-score').text(score);
-      $('#next-question').text('Correct >>');
+      else {
+        $(`input[name=answer]:checked`).parent().addClass('correct-answer');
+        // update score board in real time before route call
+        let score = $('#player-score').text();
+        score++;
+        $('#player-score').text(score);
+        $('#next-question').text('Correct >>');
+      }
     }
   });
 });
